ab42cad78feb236386fc1578dafae0c79a60afb4
Hector Torres <hectormtc@live.com>
2018-12-12 12:36:57 -0600

    Update models.py

--- inversiones/core/models.py
+++ inversiones/core/models.py
@@ -1,33 +1,97 @@
 from django.db import models
 from datetime import date
+from django.urls import reverse
+import uuid
 
-class Cliente(models.Model):
-	nombre    = models.CharField(max_length=50)
-	apellido  = models.CharField(max_length=50)
-	direccion = models.TextField()
-	celular   = models.PositiveIntegerField()
-	created_at= models.DateTimeField(auto_now_add=True)
-	Orden     = models.TextField()
-	Total_Lps = models.PositiveIntegerField()
-	deposito  = models.PositiveIntegerField()
 
-	
-	PAGADO             = 'P'
-	PENDIENTE          = 'PP'
-	ENTREGADO          = 'E'
-	ALQUILADO          = 'A'
-	ALQUILER_PENDIENTE = 'AP'
-	LOCAL              = 'L'
-	ENTREGAR           = 'EE'
-	
-	PAY     = ((PAGADO, 'Pagado'),(PENDIENTE,'Pendiente Pago'),)
-	RENT    = ((ENTREGADO, 'Entregado'), (ALQUILADO, 'En Alquiler'), (ALQUILER_PENDIENTE,'Alquiler Pendiente'),)
-	DELIVER = ((LOCAL, 'Local'),(ENTREGAR,'Entrega exterior'),)
-	Estado_de_Pago     = models.CharField(max_length=2, choices=PAY, default=PAGADO)
-	Pago_pendiente = models.PositiveIntegerField()
-	Estado_de_Renta    = models.CharField(max_length=2, choices=RENT, default=ALQUILADO)
-        Tipo_de_Entrega = models.CharField(max_length=2, choices=DELIVER, default=LOCAL)
+class Pago(models.Model):
+	pago = models.CharField(max_length=200, help_text="Especifique un tipo de pago")
 
 	def __str__(self):
-		   return '{} {} ({})'.format(
-			   self.nombre, self.apellido, self.Estado_de_Renta)
+		return self.pago
+
+
+class Estado(models.Model):
+	estado = models.CharField(max_length=200, help_text="Especifique de estado de renta")
+
+	def __str__(self):
+		return self.estado
+
+
+class Entrega(models.Model):
+	entrega = models.CharField(max_length=200, help_text="Especifique el tipo de entrega")
+
+	def __str__(self):
+		return self.entrega
+
+
+class Categoria(models.Model):
+	categoria = models.CharField(max_length=50, help_text="Tipo de categoria")
+
+	def __str__(self):
+		return self.categoria	
+
+
+class Producto(models.Model):
+	producto  = models.CharField(max_length=200, help_text="Nombre del producto")
+	detalles  = models.TextField(help_text="Detalles sobre el producto")
+	precio    = models.PositiveIntegerField()
+	cantidad  = models.PositiveIntegerField()
+	categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
+
+	def __str__(self):
+		return '{} {}'.format(
+			self.producto, self.detalles)
+
+
+class Orden(models.Model):
+        propietario = models.ForeignKey('Cliente', on_delete=models.SET_NULL, null=True)
+	observacion = models.TextField(help_text="Observacion Adicional")
+	direccion   = models.TextField()
+
+	producto = models.ManyToManyField(Producto, help_text="Ingrese el alquiler")
+
+	deposito  = models.PositiveIntegerField()
+	Total_Lps = models.PositiveIntegerField()
+
+	
+
+	def __str__(self):
+            return self.propietario
+
+	def get_absolute_url(self):
+            return reverse('cliente-detalle', args=[str(self.id)])
+
+
+class Pedido(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="ID de la orden")
+	orden = models.ForeignKey('Orden', on_delete_models.SET_NULL, null=True)
+
+	date_deliver = models.DateField("Fecha de entrega", null=True, blank=True, help_text="Fecha en que se entregara el pedido")
+	date_receive = models.DateField("Fecha de retorno", null=True, blank=True, help_text="Fecha en que se debe recibir el pedido)
+	date_return = models.DateField("Fecha de devolucion", null=True, blank=True, help_text="Fecha en que el cliente devolvio el pedido")
+
+	entrega = models.ManyToManyField(Entrega, help_text="Tipo de Entrega")
+	estado  = models.ManyToManyField(Estado, help_text="Estado de Alquiler")
+	pago    = models.ManyToManyField(Pago, help_text="Estado de Pago")
+
+	class Meta:
+		ordering = ["date_deliver"]
+
+	def __str__(self):
+		return '%s (%s)' % (self.id, self.
+
+
+class Cliente(models.Model):
+	nombre    = models.CharField(max_length=100)
+	apellido  = models.CharField(max_length=100)
+	celular   = models.PositiveIntegerField()
+
+	def __str__(self):
+		   return '{} {}'.format(
+			   self.nombre, self.apellido)
+	
+	def get_abosulte_url(self):
+		return reverse('cliente-detalle', args=[str(self.id)])
+	
+	
