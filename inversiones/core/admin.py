7aa82dc667ac2f9b95eefcb828b23395e1bae856
Hector Torres <hectormtc@live.com>
2018-12-20 11:13:59 -0600

    Add files via upload

--- inversiones/core/admin.py
+++ inversiones/core/admin.py
@@ -1,9 +1,85 @@
 from django.contrib import admin
-from .models import Categoria,Producto,Orden,Pedido,Cliente,Renta
+from .models import Categoria,Producto,Pedido,Cliente,Renta
 
-admin.site.register(Pedido)
-admin.site.register(Orden)
+
+#admin.site.register(Cliente)
+#admin.site.register(Pedido)
+#admin.site.register(Producto)
+#admin.site.register(Renta)
+
+
+class ClienteAdmin(admin.ModelAdmin):
+	list_display = ('nombre',
+			'apellido',
+			'celular',
+			'empresa')
+	list_filter = ('empresa',)
+
+admin.site.register(Cliente, ClienteAdmin)
+
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+	list_display = ('producto',
+			'detalles',
+			'precio',
+			'cantidad',
+			'display_Categoria')
+
+	list_filter = ('categoria',)
+
+	fields = [('producto','detalles'),
+		('precio','cantidad','categoria')]
+
+
+class RentaInLine(admin.TabularInline):
+	model = Renta
+
+"""
+@admin.register(Renta)
+class RentaAdmin(admin.ModelAdmin):
+	list_display = ('producto','cantidad')
+	inLines = [RentaInLine]
+"""
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+	inLines = [RentaInLine]
+	list_display  = ('cliente',
+			'estado_de_renta',
+			'date_deliver',
+			'date_receive')
+	
+	list_filter = ('estado_de_renta',
+			'tipo_de_entrega',
+			'pago_pendiente',
+			'estado_de_pago')
+	
+	
+	fieldsets = (
+		(None, {
+			'fields':['cliente',
+				'renta']
+			}),
+		('Control de fechas',{
+			'fields':[('date_deliver',
+				'date_receive',
+				'date_return')]
+			}),
+		('Estados del pedido',{
+			'fields':[('estado_de_renta',
+				'estado_de_pago',
+				'tipo_de_entrega')]
+			}),
+		('Pendientes',{
+			'fields':[('pago_pendiente',
+				'deposito')]
+			}),
+		('Datos Adicionales',{
+			'fields':('direccion',
+				'observacion')
+			}),
+		)
+
 admin.site.register(Categoria)
-admin.site.register(Producto)
-admin.site.register(Cliente)
-admin.site.register(Renta)
+
--- inversiones/core/models.py
+++ inversiones/core/models.py
@@ -11,11 +11,15 @@ class Categoria(models.Model):
 
 
 class Producto(models.Model):
-	producto    = models.CharField('Producto', max_length=200, help_text="Nombre del producto")
-	detalles  = models.TextField(help_text="Detalles sobre el producto",blank=True)
+	producto  = models.CharField('Producto', max_length=200, help_text="Nombre del producto")
+	detalles  = models.CharField(max_length=200, help_text="Detalles sobre el producto",blank=True)
 	precio    = models.PositiveIntegerField()
 	cantidad  = models.PositiveIntegerField()
 	categoria = models.ForeignKey('Categoria', on_delete=models.SET_NULL, null=True)
+
+	def display_Categoria(self):
+		return ', '.join([ self.categoria.categoria])
+		display_Categoria.short_description = 'Categoria'
 
 	def __str__(self):
 		return '{0} {1}'.format(self.producto, self.detalles)
@@ -23,53 +27,17 @@ class Producto(models.Model):
 class Renta(models.Model):
 	producto = models.ForeignKey('Producto', on_delete=models.SET_NULL, null=True)
 	cantidad = models.PositiveIntegerField()
-
+"""
 	def getSubtotal(self):
 		return int(self.cantidad * self.producto.precio)
 
 	def __str__(self):
-		return 'Producto: {0}, Cantidad: {1} Subtotal: {2}'.format(
-													self.producto,
-													self.cantidad,
-													self.getSubtotal())
-
-class Cliente(models.Model):
-	nombre    = models.CharField(max_length=100)
-	apellido  = models.CharField(max_length=100)
-	celular   = models.BigIntegerField()
-
-
-	def get_abosulte_url(self):
-		return reverse('cliente-detalle', args=[str(self.id)])
-
-
-	def __str__(self):
-		   return '{0} {1}'.format(
-			   self.nombre, self.apellido)
-
-
-class Orden(models.Model):
-
-	pass
-
-	"""def getTotal(self):
-		return sum(self.renta.getSubtotal() * self.renta)
-
-	def __str__(self):
-		return 'Cliente {0} {1}'.format(
-									self.propietario.nombre, self.propietario.apellido)
-									#self.getTotal())
-
-	def get_absolute_url(self):
-		return reverse('cliente-detalle', args=[str(self.id)])
+		return 'Producto: {0}, Cantidad: {1} Subtotal: {2}'.format(self.producto,self.cantidad,self.getSubtotal())
 """
 
 class Pedido(models.Model):
-	propietario = models.ForeignKey('Cliente', on_delete=models.SET_NULL, null=True)
-	#id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="ID de la orden")
+	cliente = models.ForeignKey('Cliente', on_delete=models.SET_NULL, null=True)
 	renta = models.ForeignKey('Renta', on_delete=models.SET_NULL, null=True)
-
-	observacion = models.TextField(help_text="Observacion Adicional", blank=True)
 
 	date_deliver = models.DateField("Fecha de entrega", null=True, blank=True, help_text="Fecha en que se entregara el pedido")
 	date_receive = models.DateField("Fecha de retorno", null=True, blank=True, help_text="Fecha en que se debe recibir el pedido")
@@ -87,12 +55,13 @@ class Pedido(models.Model):
 	RENT    = ((ENTREGADO, 'Entregado'), (ALQUILADO, 'En Alquiler'), (ALQUILER_PENDIENTE,'Alquiler Pendiente'),)
 	DELIVER = ((LOCAL, 'Local'),(ENTREGAR,'Entrega exterior'),)
 
-	estado_de_Pago  = models.CharField(max_length=2, choices=PAY, default=PAGADO)
-	pago_pendiente  = models.PositiveIntegerField(blank=True)
-	deposito        = models.PositiveIntegerField(blank=True)
+	estado_de_pago  = models.CharField(max_length=2, choices=PAY, default=PAGADO)
 	estado_de_renta = models.CharField(max_length=2, choices=RENT, default=ALQUILADO)
 	tipo_de_entrega = models.CharField(max_length=2, choices=DELIVER, default=LOCAL)
+	pago_pendiente  = models.PositiveIntegerField(blank=True)
+	deposito        = models.PositiveIntegerField(blank=True)
 	direccion       = models.TextField(help_text='Solo en caso de entrega exterior', blank=True)
+	observacion = models.TextField(help_text="Observacion Adicional", blank=True)
 
 
 	class Meta:
@@ -100,4 +69,18 @@ class Pedido(models.Model):
 
 
 	def __str__(self):
-		return '%s' % (self.estado_de_renta)
+		return '%s' % (self.cliente.nombre)
+
+class Cliente(models.Model):
+	nombre    = models.CharField(max_length=100)
+	apellido  = models.CharField(max_length=100, blank=True)
+	celular   = models.BigIntegerField()
+	empresa   = models.BooleanField()
+
+
+	def get_abosulte_url(self):
+		return reverse('cliente-detalle', args=[str(self.id)])
+
+
+	def __str__(self):
+		   return '{0} {1}'.format(self.nombre, self.apellido)
