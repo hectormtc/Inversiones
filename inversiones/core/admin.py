845396a2efbbad0475de758fec1a1488feb1ef2d
Hector Torres <hectormtc@live.com>
2020-02-28 09:20:41 -0600

    Add files via upload

--- inversiones/core/admin.py
+++ inversiones/core/admin.py
@@ -1,83 +1,59 @@
 from django.contrib import admin
-from .models import Categoria,Producto,Pedido,Cliente,Renta
+from .models import Phone, Address, Categoria,Producto,Cliente, Encargado, Orden, Factura,Inventario,Articulo, Rol, FacturaInstance, ArticuloInstance
 
 
-#admin.site.register(Cliente)
-#admin.site.register(Pedido)
-#admin.site.register(Producto)
-#admin.site.register(Renta)
+class ArticuloInLine(admin.TabularInline):
+    model = ArticuloInstance
+@admin.register(Articulo)
 
+class ArticuloAdmin(admin.ModelAdmin):
+    list_display = ('producto', 'cantidad')
+    inlines = [ArticuloInLine]
+###########################################
+@admin.register(Orden)
+class OrdenAdmin(admin.ModelAdmin):
+    list_display = ('articulo',)
+
+#############################################
+class FacturaInstanceInLine(admin.TabularInline):
+    model =  FacturaInstance
+###########################################33#
+@admin.register(Factura)
+class FacturaAdmin(admin.ModelAdmin):
+    list_display  = ('cliente',)
+    inlines = [FacturaInstanceInLine]
+
+class FacturaInstanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'orden',
+    'estado_de_pago'
+    'estado_de_renta',
+    'date_deliver',
+    'date_receive')
+    list_filter = ('estado_de_renta',
+    'tipo_de_entrega',
+    'pago_pendiente',
+    'estado_de_pago')
+    search_fields = ('nombre','apellido','empresa')
+################################################33
 
 class ClienteAdmin(admin.ModelAdmin):
 	list_display = ('nombre',
 			'apellido',
-			'celular',
 			'empresa')
 	list_filter = ('empresa',)
+	search_fields = ('nombre','apellido','empresa')
 
 admin.site.register(Cliente, ClienteAdmin)
+#######################################################
 
-
-@admin.register(Producto)
-class ProductoAdmin(admin.ModelAdmin):
-	list_display = ('producto',
-			'detalles',
-			'precio',
-			'cantidad',
-			'display_Categoria')
-
-	list_filter = ('categoria',)
-
-	fields = [('producto','detalles'),
-		('precio','cantidad','categoria')]
-
-
-class RentaInLine(admin.TabularInline):
-	model = Renta
-
-@admin.register(Renta)
-class RentaAdmin(admin.ModelAdmin):
-	
-	list_display = ('producto','cantidad')
-
-
-
-@admin.register(Pedido)
-class PedidoAdmin(admin.ModelAdmin):
-	list_display  = ('cliente',
-			'estado_de_renta',
-			'date_deliver',
-			'date_receive')
-	
-	list_filter = ('estado_de_renta',
-			'tipo_de_entrega',
-			'pago_pendiente',
-			'estado_de_pago')
-"""
-	fieldsets = (
-		(None, {
-			'fields':['id','cliente',
-				'renta']
-			}),
-		('Control de fechas',{
-			'fields':[('date_deliver',
-				'date_receive',
-				'date_return')]
-			}),
-		('Estados del pedido',{
-			'fields':[('estado_de_renta',
-				'estado_de_pago',
-				'tipo_de_entrega')]
-			}),
-		('Pendientes',{
-			'fields':[('pago_pendiente',
-				'deposito')]
-			}),
-		('Datos Adicionales',{
-			'fields':('direccion',
-				'observacion')
-			}),
-		)
-"""
+admin.site.register(Phone)
+admin.site.register(Address)
+admin.site.register(Rol)
+admin.site.register(Inventario)
+#admin.site.register(Articulo)
+#admin.site.register(Factura)
+admin.site.register(Encargado)
+#admin.site.register(Orden)
+#admin.site.register(Cliente)
+admin.site.register(Producto)
 admin.site.register(Categoria)
-
